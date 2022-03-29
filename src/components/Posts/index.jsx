a3c665dc333562e904ad591f99c8cc8ae23f9890
Matheus Henrique <matehenrique20@hotmail.com>
2022-03-29 00:32:06 -0300

    first commit

---
+++ src/components/Posts/index.jsx
@@ -0,0 +1,17 @@
+import { Postcard } from "../PostCard"
+
+import './styles.css'
+
+export const Posts = ({ posts }) => (
+    <div className="posts">
+          {posts.map(post => (
+          <Postcard
+            key={post.id}
+            title={post.title}
+            body={post.body}
+            id={post.id}
+            cover={post.cover}
+          />
+        ))}
+      </div>
+)
